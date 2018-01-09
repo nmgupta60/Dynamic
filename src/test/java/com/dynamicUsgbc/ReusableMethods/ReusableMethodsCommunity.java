46266a5a642f7e63beb2a6658cd1e310e3763382
Abhishek-Digha <abhishek.digha1990@gmail.com>
2018-01-09 14:30:04 +0530

    Test

--- src/test/java/com/dynamicUsgbc/ReusableMethods/ReusableMethodsCommunity.java
+++ src/test/java/com/dynamicUsgbc/ReusableMethods/ReusableMethodsCommunity.java
@@ -1,5 +1,7 @@
 package com.dynamicUsgbc.ReusableMethods;
 
+import java.io.IOException;
+
 import com.dynamicUsgbc.driver.BaseClass;
 import com.dynamicUsgbc.driver.CommonMethod;
 
@@ -21,11 +23,17 @@ public class ReusableMethodsCommunity extends BaseClass {
 			String graduationDate  = data.getCellData(sheetName, "GraduationDate", rowNum);
 			String dob       = data.getCellData(sheetName, "DOB", rowNum);
 			
-			CommonMethod.selectdropdown("communityName",);
-			CommonMethod.testlog("Pass", "Feeding Random Unique name for Project and saving to external location");
+			CommonMethod.selectdropdown("communityName",communityName);
+			CommonMethod.testlog("Pass", "Selecting the community Name frm the Dropdown");
 			
-			CommonMethod.selectdropdown("ClickOnProjectType",prjType);
-			CommonMethod.testlog("Pass","Selecting Project Type as Buildings");
+			CommonMethod.selectdropdown("communityCountry",country);
+			CommonMethod.testlog("Pass","Selecting the Country Name");
+			
+			CommonMethod.sendKeys("CommunityAdd1", street1);
+			CommonMethod.sendKeys("CommunityAdd2", street2);
+			CommonMethod.sendKeys("CommunityCity", city);
+			CommonMethod.selectdropdown("CommunityState", state);
+			CommonMethod.sendKeys("", );
 			
 		
 		}
